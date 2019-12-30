b3d310be38afd3a18a8e946a4f3663db031fc32d
kji000213 <fenglong0228@126.com>
2019-12-30 18:12:36 +0800

    “我的底栏提交”

---
+++ pages/my/my.vue
@@ -0,0 +1,149 @@
+<template>
+	<view>
+		<view class="content">
+			<view class="joiList" v-for="item in datalist" :key="item.usr_id"  @click="goto(item.id)" >
+
+
+
+			        <view class='weWzpad' >
+			            <p class='font14'>用户名：{{item.username}}</p>
+			            <p class='font14'>登录账号：{{item.loginname}}</p>
+                         <p class='font14'>部门id：{{item.deptid}}</p>
+			        </view>
+
+
+			</view>
+		</view>
+
+	</view>
+</template>
+
+<script>
+   import {getHandle} from "../../common/api.js"
+	import uniLoadMore from "@/components/uni-load-more/uni-load-more.vue"
+	export default {
+		 components: {uniLoadMore},
+		data() {
+			return {
+                  deptid:"",
+                   loginname:"",
+                   username:""
+			}
+		},
+		methods: {
+
+		},
+
+		onLoad() {
+            let that=this
+           uni.getStorage({
+               key: 'userdata',
+               success: function (res) {
+
+                   that.deptid=res.data.deptid;
+                   that.loginname=res.data.loginname;
+                   that.username=res.data.username
+
+               }
+           });
+
+	   },
+	}
+</script>
+
+<style>
+.joiList {
+    padding: 17px 15px 9px;
+    display: flex;
+	/* border: 1px solid red; */
+	background: #fff;
+	border-bottom: 1px solid #eee;
+
+
+
+}
+
+.serBox {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+.serindWid {
+    width: 100%;
+}
+.propFlex {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.propFlex1 {
+    display: flex;
+    justify-content: space-between;
+}
+.num {
+    font-size: 15px;
+    color: #898989;
+}
+.invdiff {
+    padding-top: 18px;
+}
+.comWaiting {
+    color: #2B76FB;
+    font-size: 16px;
+}
+
+.joiList {
+    padding: 17px 15px 9px;
+    display: flex;
+}
+
+.joiPad {
+    padding: 15px 0;
+    border-top: 1px solid #eeeeee;
+    flex: 1;
+}
+
+.comFin {
+    color: #5edba8;
+    font-size: 15px;
+}
+
+.comNo {
+    color: #ea595d;
+    font-size: 15px;
+}
+.serDrop {
+    padding-bottom: 10px;
+}
+
+.serDrop1 {
+    padding-bottom: 5px;
+}
+.myserveWz {
+    font-size: 18px;
+    color: #898989;
+}
+
+.actWz {
+    font-size: 16px;
+    color: #333333;
+}
+.proFont {
+    font-weight: 600;
+}
+.projectBox {
+    margin-top: 5px;
+	display: flex;
+	justify-content: flex-end;
+}
+.weWzpad {
+    padding-top: 12px;
+}
+.font14 {
+    font-size: 14px;
+    color: #898989;
+}
+.myPadtop {
+    padding-top: 5px;
+}
+</style>
